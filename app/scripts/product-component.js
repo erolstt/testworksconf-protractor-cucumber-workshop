2ddda21ea54abfe9261b24cba0eb839b2a213307
Edze Knol <eknol@xebia.com>
2015-09-15 22:22:53 +0200

    added xebia training products

--- app/scripts/product-component.js
+++ app/scripts/product-component.js
@@ -14,14 +14,14 @@ function ProductController($scope, ngCart) {
   var ctrl = this;
 
   ctrl.products = [
-    new Product(1, 'Product 1', 'A very cool product', 'product-1.jpg', 9.95),
-    new Product(2, 'Product 2', 'A mediocre product', 'product-2.jpg', 6.95),
-    new Product(3, 'Product 3', 'Cheap but efficient', 'product-3.jpg', 4.95),
-    new Product(4, 'Product 4', 'Cheap but efficient', 'product-4.jpg', 4.95),
-    new Product(5, 'Product 5', 'Cheap but efficient', 'product-5.jpg', 4.95),
-    new Product(6, 'Product 6', 'Cheap but efficient', 'product-6.jpg', 4.95),
-    new Product(7, 'Product 7', 'Cheap but efficient', 'product-7.jpg', 4.95),
-    new Product(8, 'Product 8', 'Cheap but efficient', 'product-8.jpg', 4.95)
+    new Product(1, 'Programming for Testers by Alan Richardson', 'Get out of your comfort zone and improve your Test Automation and programming skills in all phases of software development.', 'logo.svg', 1495),
+    new Product(2, 'Testing 3.0', 'Are you ready for testing 3.0?', 'logo.svg', 795),
+    new Product(3, 'Testing in AngularJS', 'In this training you will learn about writing and maintainable tests for AngularJS applications.', 'logo.svg', 695),
+    new Product(4, 'Behaviour Driven Development with Cucumber', 'This training will help teams understand the true meaning of BDD and how to use Cucumber properly.', 'logo.svg', 695),
+    new Product(5, 'Advanced Selenium WebDriver', 'This training will help developers and testers become more advanced in using Selenium WebDriver.', 'logo.svg', 695),
+    new Product(6, 'Specification by Example', 'This training helps teams to improve collaboration in all phases of software development.', 'logo.svg', 795),
+    new Product(7, 'Test Driven Development (TDD)', 'If you don\'t know how to test it, you have no business building it.', 'logo.svg', 495),
+    new Product(8, 'Unit Testing in Visual studio 2013', 'This training will learn you to effectively use Visual Studio 2013 to design, write, and run high-quality .NET unit tests.', 'logo.svg', 1250)
   ];
 
   ctrl.displayProductAddedAlert = false;
