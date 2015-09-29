cffa0e8b7f2c579925af6fba9a367583fffceac5
Marc Rooding <admin@webresource.nl>
2015-09-29 14:14:31 +0200

    Removed 2 unused methods

--- e2e/pages/shopping-cart.js
+++ e2e/pages/shopping-cart.js
@@ -4,9 +4,6 @@ module.exports = {
   $cartEmptyAlert: element(by.css('[data-e2e-id="cart-empty-alert"]')),
   $cartProductTotalPrice: element(by.css('[data-e2e-id="cart-product-total-price"]')),
   $cartRemoveProductsButton: element.all(by.css('[data-e2e-id="cart-remove-product"]')),
-  removeProductFromCart: function (index) {
-    this.$cartRemoveProductsButton.get(index).click();
-  },
   $cartTotalPrice: element(by.css('[data-e2e-id="cart-total-price"]')),
   getProduct: function (index) {
     var product = element.all(by.css('[data-e2e-id="cart-product"]')).get(index);
@@ -15,16 +12,6 @@ module.exports = {
       name: product.element(by.css("[data-e2e-id='cart-product'] td:nth-child(2)")),
       quantity: product.element(by.css("[data-e2e-id='cart-product'] td:nth-child(3)")),
       totalPrice: product.element(by.css("[data-e2e-id='cart-product'] td:nth-child(5)"))
-    };
-  },
-  getProductNew: function (name) {
-    var product = element(by.css("[data-product-name=\'" + name + "\']"));
-    return {
-      product: product,
-      name: product.element(by.css("[data-e2e-id='cart-product'] td:nth-child(2)")),
-      quantity: product.element(by.css("[data-e2e-id='cart-product'] td:nth-child(3)")),
-      totalPriceProduct: product.element(by.css("[data-e2e-id='cart-product'] td:nth-child(5)")),
-      removeProduct: product.element(by.css('[data-e2e-id="cart-remove-product"]'))
     };
   },
   $cartProducts: element.all(by.css('[data-e2e-id="cart-product"]')),
